194a8adfdcb7e55f8096907402cd2d47e6917ccd
Michael John Gatab <michaeljohn.gatab@asti.dost.gov.ph>
2025-10-30 19:50:22 +0000

    end of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/products';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  protected readonly title = signal('client');
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
+  protected readonly title = signal('Skinet');
+  products = signal<Product[]>([]);
+
+  ngOnInit():void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products.set(response.data),
+      error: error => console.error('There was an error!', error),
+      complete: () => console.log('complete')
+    });
+  }
 }
